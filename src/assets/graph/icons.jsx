c9d17b6e4bc3117ea150a61a59f8a91599d3d42a
leeboa2005 <leeboa2005@naver.com>
2024-05-29 12:43:04 +0900

    update : assets/graph/incons.jsx 추가

---
+++ src/assets/graph/icons.jsx
@@ -0,0 +1,33 @@
+import { IoFastFoodOutline } from 'react-icons/io5';
+import { IoBookOutline } from 'react-icons/io5';
+import { PiHairDryer } from 'react-icons/pi';
+import { LuCandy } from 'react-icons/lu';
+import { MdTravelExplore } from 'react-icons/md';
+import { MdCastForEducation } from 'react-icons/md';
+import { BiDumbbell } from 'react-icons/bi';
+import { GiRolledCloth } from 'react-icons/gi';
+import { LuPartyPopper } from 'react-icons/lu';
+import { MdHealthAndSafety } from 'react-icons/md';
+import { MdMovieFilter } from 'react-icons/md';
+import { TiShoppingCart } from 'react-icons/ti';
+import { FaBus } from 'react-icons/fa6';
+import { FaMinus } from 'react-icons/fa'; // 기타 아이콘
+const icons = {
+    미용: PiHairDryer,
+    도서: IoBookOutline,
+    의류비: GiRolledCloth,
+    식비: IoFastFoodOutline,
+    간식: LuCandy,
+    여행: MdTravelExplore,
+    교육: MdCastForEducation,
+    운동: BiDumbbell,
+    경조사: LuPartyPopper,
+    건강: MdHealthAndSafety,
+    의료: MdHealthAndSafety,
+    문화생활: MdMovieFilter,
+    생필품: TiShoppingCart,
+    교통: FaBus,
+    기타: FaMinus,
+};
+
+export default icons;
