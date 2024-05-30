ac66f4d0f9e0faf7d6130912cb38d0ac5312acad
leeboa2005 <leeboa2005@naver.com>
2024-05-30 15:21:03 +0900

    불필요한 props 제거

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addExpenseData } from '../redux/modules/expenseDate';
 import styled from 'styled-components';
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -4,31 +4,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Detail from '../pages/Detail';
 import Layout from '../shared/Layout';
-import PropTypes from 'prop-types';
 
-const Router = ({ expenseData, setExpenseData }) => {
+const Router = () => {
     return (
         <BrowserRouter>
             <Layout>
                 <Routes>
-                    {/* 홈 페이지 */}
-                    <Route path="/" element={<Home expenseData={expenseData} setExpenseData={setExpenseData} />} />
-
-                    {/* 디테일 페이지 */}
-                    <Route
-                        path="/detail/:id"
-                        element={<Detail expenseData={expenseData} setExpenseData={setExpenseData} />}
-                    />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/detail/:id" element={<Detail />} />
                 </Routes>
             </Layout>
         </BrowserRouter>
     );
-};
-
-// PropType 검증
-Router.propTypes = {
-    expenseData: PropTypes.array.isRequired,
-    setExpenseData: PropTypes.func.isRequired,
 };
 
 export default Router;
